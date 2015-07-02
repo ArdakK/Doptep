d87bbe8cb79d51d426a5b1ae21186a90f5118f14
ArdakK <ardakthemathematician@mail.ru>
2015-07-02 10:55:05 +0600

    Initial Commit

---
+++ pitch 0.1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  pitch 0.1
+//
+//  Created by WhiteWind on 02.07.15.
+//  Copyright (c) 2015 WhiteWind. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ pitch 0.1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  pitch 0.1
+//
+//  Created by WhiteWind on 02.07.15.
+//  Copyright (c) 2015 WhiteWind. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
